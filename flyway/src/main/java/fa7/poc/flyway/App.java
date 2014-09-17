a40b2a3f7612b243fcd161408d449d898b512a39
Valterfi <morpheuw@gmail.com>
2014-09-16 21:19:46 -0300

    primeiro commit

---
+++ flyway/src/main/java/fa7/poc/flyway/App.java
@@ -0,0 +1,13 @@
+package fa7.poc.flyway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
